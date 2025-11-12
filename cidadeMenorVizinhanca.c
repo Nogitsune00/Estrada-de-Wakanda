34e73114297c3f9bfb841522b398445111db0e0e
ARTHURR1898 <arthur.ramalho1234@gmail.com>
2025-11-11 21:10:59 -0300

    update cidadeMenorVizinhanca

--- cidadeMenorVizinhanca.c
+++ cidadeMenorVizinhanca.c
@@ -10,14 +10,12 @@ char *cidadeMenorVizinhanca(const char *nomeArquivo) {
         return NULL;
     }
 
-    int T, N;
-    if (fscanf(arq, "%d", &T) != 1 || fscanf(arq, "%d", &N) != 1) {
-        fclose(arq);
-        return NULL;
-    }
+    int T, N; // T = comprimento total; N = número de cidades
+    fscanf(arq, "%d", &T);
+    fscanf(arq, "%d", &N);
 
-    int *pos = malloc(N * sizeof(int));
-    char (*nomes)[256] = malloc(N * sizeof(*nomes));
+    int *pos = (int*) malloc(N * sizeof(int));
+    char (*nomes)[256] = malloc(N * sizeof(*nomes)); // guarda o nome de cada cidade
     if (!pos || !nomes) {
         fclose(arq);
         free(pos);
@@ -26,25 +24,12 @@ char *cidadeMenorVizinhanca(const char *nomeArquivo) {
     }
 
     for (int i = 0; i < N; i++) {
-        if (fscanf(arq, "%d", &pos[i]) != 1) {
-            fclose(arq);
-            free(pos);
-            free(nomes);
-            return NULL;
-        }
-
-        while (fgetc(arq) == ' ');
-
-        fseek(arq, -1, SEEK_CUR);
-        fgets(nomes[i], 256, arq);
-
-        nomes[i][strcspn(nomes[i], "\r\n")] = 0;
-
-        while (nomes[i][0] == ' ')
+        fscanf(arq, "%d %[^\n]", &pos[i], nomes[i]);
+        while (nomes[i][0] == ' ') // remove espaço inicial, se houver
             memmove(nomes[i], nomes[i] + 1, strlen(nomes[i]));
     }
-    fclose(arq);
 
+    // Ordena as cidades por posição (mesmo código da sua amiga)
     for (int i = 0; i < N - 1; i++) {
         for (int j = i + 1; j < N; j++) {
             if (pos[j] < pos[i]) {
@@ -60,29 +45,40 @@ char *cidadeMenorVizinhanca(const char *nomeArquivo) {
         }
     }
 
-    double menorViz = 1e9;
+    // Mantém a estrutura do código dela, só com cálculo ajustado
+    double menorViz = 999999.0;
+    double vizinhoAnterior = 0.0;
     int idxMenor = 0;
 
-    for (int i = 0; i < N; i++) {
-        double viz;
-        if (i == 0)
-            viz = (pos[i + 1] - 0) / 2.0;
-        else if (i == N - 1)
-            viz = (T - pos[i - 1]) / 2.0;
-        else
-            viz = (pos[i + 1] - pos[i - 1]) / 2.0;
+    for (int i = 1; i <= N; i++) {
+        double vizinhanca = 0.0;
+        double dif = 0.0;
 
-        if (viz < menorViz) {
-            menorViz = viz;
-            idxMenor = i;
+        if (i < N) {
+            vizinhanca = pos[i-1] + ((pos[i] - pos[(i - 1)]) / 2.0);
+            dif = vizinhanca - vizinhoAnterior;
+        } else {
+            vizinhanca = (T - vizinhoAnterior) / 1.0;
+            dif = T - vizinhoAnterior;
         }
+
+        if (dif < menorViz) {
+            menorViz = dif;
+            idxMenor = i - 1; // 👈 guarda o índice da cidade
+        }
+
+        vizinhoAnterior = vizinhanca;
     }
 
+    fclose(arq);
+
+    // Cria a string pra retornar o nome da cidade
     char *resultado = malloc(strlen(nomes[idxMenor]) + 1);
-    if (resultado)
+    if (resultado != NULL)
         strcpy(resultado, nomes[idxMenor]);
 
     free(pos);
     free(nomes);
+
     return resultado;
 }
